b6717940bcc9b5ef1cbea571283b5278a57951da
raghuram1015 <raghuramp456@gmail.com>
2025-10-04 02:15:21 +0530

    Functions Update

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,21 +1,22 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyAGUYloq3Ktq5POWrTEj8ui7pHaNW3poBE",
-    authDomain: "session-booker-5422.firebaseapp.com",
-    projectId: "session-booker-5422",
-    storageBucket: "session-booker-5422.firebasestorage.app",
-    messagingSenderId: "942763853117",
-    appId: "1:942763853117:web:bb82beb454ffea850cf154",
-    measurementId: "G-7C3Z463NW4"
-  };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAGUYloq3Ktq5POWrTEj8ui7pHaNW3poBE",
+  authDomain: "session-booker-5422.firebaseapp.com",
+  projectId: "session-booker-5422",
+  storageBucket: "session-booker-5422.firebasestorage.app",
+  messagingSenderId: "942763853117",
+  appId: "1:942763853117:web:bb82beb454ffea850cf154",
+  measurementId: "G-7C3Z463NW4"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export the services that other files in your project need
+export const auth = getAuth(app);
+export const db = getFirestore(app);
